cc7e0a3e1a020c28f3a5d33c2724cba2ce65cff9
Kanagat <k_sapiya@kbtu.kz>
2022-11-23 11:17:20 +0600

    task2 added

--- week11/task2_quiz/script.js
+++ week11/task2_quiz/script.js
@@ -1,29 +1,24 @@
 (function () {
   var questions = [
     {
-      question: "What is 1+1?",
-      choices: [2, 5, 10, 15, 20],
+      question: "What's 2+2?",
+      choices: [2, 4, 1, 3, 13],
+      correctAnswer: 1,
+    },
+    {
+      question: "What's 3+6?",
+      choices: [3, 6, 9, 12, 18],
       correctAnswer: 2,
     },
     {
-      question: "What is 1+2?",
-      choices: [3, 6, 9, 12, 18],
-      correctAnswer: 3,
-    },
-    {
-      question: "What is 123456*0?",
-      choices: [0, 0, 0, 0, 1],
+      question: "What's 123*0?",
+      choices: [0, 6, 8, 4, 1],
       correctAnswer: 0,
     },
     {
-      question: "What is 9^2?",
-      choices: [64, 55, 63, 75, 81],
-      correctAnswer: 81,
-    },
-    {
-      question: "What is 8*8?",
-      choices: [20, 30, 40, 50, 64],
-      correctAnswer: 64,
+      question: "What's my height?",
+      choices: [175, 178, 160, 183, 177],
+      correctAnswer: 3,
     },
   ];
 
@@ -31,41 +26,38 @@
   var selections = [];
   var quiz = $("#quiz");
 
-  // Display initial question
   displayNext();
 
-  // Click handler for the 'next' button
   $("#next").on("click", function (e) {
     e.preventDefault();
 
-    // Suspend click listener during fade animation
     if (quiz.is(":animated")) {
       return false;
     }
     choose();
 
-    // If no user selection, progress is stopped
     if (isNaN(selections[questionCounter])) {
-      alert("Please vybery kakoy libo otvet!");
+      alert("Please make a selection!");
     } else {
       questionCounter++;
       displayNext();
     }
   });
 
-  // Click handler for the 'prev' button
   $("#prev").on("click", function (e) {
     e.preventDefault();
 
     if (quiz.is(":animated")) {
       return false;
     }
+
     choose();
+
     questionCounter--;
+
     displayNext();
   });
 
-  // Click handler for the 'Start Over' button
   $("#start").on("click", function (e) {
     e.preventDefault();
 
@@ -78,7 +70,6 @@
     $("#start").hide();
   });
 
-  // Animates buttons on hover
   $(".button").on("mouseenter", function () {
     $(this).addClass("active");
   });
@@ -86,14 +77,12 @@
     $(this).removeClass("active");
   });
 
-  // Creates and returns the div that contains the questions and
-  // the answer selections
   function createQuestionElement(index) {
     var qElement = $("<div>", {
       id: "question",
     });
 
-    var header = $("<h2>Question " + (index + 1) + ":</h2>");
+    var header = $("<h2>Vopros #" + (index + 1) + ":</h2>");
     qElement.append(header);
 
     var question = $("<p>").append(questions[index].question);
@@ -105,11 +94,11 @@
     return qElement;
   }
 
-  // Creates a list of the answer choices as radio inputs
   function createRadios(index) {
     var radioList = $("<ul>");
     var item;
     var input = "";
+
     for (var i = 0; i < questions[index].choices.length; i++) {
       item = $("<li>");
       input = '<input type="radio" name="answer" value=' + i + " />";
@@ -120,12 +109,10 @@
     return radioList;
   }
 
-  // Reads the user selection and pushes the value to an array
   function choose() {
     selections[questionCounter] = +$('input[name="answer"]:checked').val();
   }
 
-  // Displays next requested element
   function displayNext() {
     quiz.fadeOut(function () {
       $("#question").remove();
@@ -140,7 +127,6 @@
           );
         }
 
-        // Controls display of 'prev' button
         if (questionCounter === 1) {
           $("#prev").show();
         } else if (questionCounter === 0) {
@@ -149,6 +135,7 @@
         }
       } else {
         var scoreElem = displayScore();
+
         quiz.append(scoreElem).fadeIn();
         $("#next").hide();
         $("#prev").hide();
@@ -157,7 +144,6 @@
     });
   }
 
-  // Computes score and returns a paragraph element to be displayed
   function displayScore() {
     var score = $("<p>", { id: "question" });
 
@@ -168,13 +154,8 @@
       }
     }
 
-    score.append(
-      "You got " +
-        numCorrect +
-        " questions out of " +
-        questions.length +
-        " right!!!"
-    );
+    score.append("Your score is " + numCorrect);
+
     return score;
   }
 })();
